2e7709f385fca2e83e94ce16d1fcd3978974f678
Virgi Ghali Putra <virgighali@gmail.com>
2020-04-02 20:10:36 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HeaderGhal/>
+    <HeroGhal/>
+    <WomenBanner/>
+    <InstaGhal/>
+    <PartnerGhal/>
+    <FooterGhal/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+import HeaderGhal from '@/components/HeaderGhal.vue'
+import HeroGhal from '@/components/HeroGhal.vue'
+import WomenBanner from '@/components/WomenBanner.vue'
+import InstaGhal from '@/components/InstaGhal.vue'
+import PartnerGhal from '@/components/PartnerGhal.vue'
+import FooterGhal from '@/components/FooterGhal.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderGhal,
+    HeroGhal,
+    WomenBanner,
+    InstaGhal,
+    PartnerGhal,
+    FooterGhal
+  }
+};
+</script>
